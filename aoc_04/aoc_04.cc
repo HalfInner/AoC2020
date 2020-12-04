89238fc4351bfbb74196252633aad24a008691f0
Half's Inner <kajbrz@hotmail.com>
2020-12-04 11:39:45 +0100

    Boost

--- aoc_04/aoc_04.cc
+++ aoc_04/aoc_04.cc
@@ -11,13 +11,89 @@
 
 #include "HolidayBag.hh"
 
+template <typename Passport>
+bool hasValidFieldNumber(Passport &&passport) {
+  return (passport.size() == 8) ||
+         (passport.size() == 7 && (passport.find("cid") == cend(passport)));
+}
+
+template <typename Passport>
+bool hasValidFieldByr(Passport &&passport) {
+  if (auto it = passport.find("byr"); it != cend(passport)) {
+    const auto year = std::stoi(it->second);
+    return year >= 1920 && year <= 2002;
+  }
+  return false;
+}
+
+template <typename Passport>
+bool hasValidFieldIyr(Passport &&passport) {
+  if (auto it = passport.find("iyr"); it != cend(passport)) {
+    const auto year = std::stoi(it->second);
+    return year >= 2010 && year <= 2020;
+  }
+  return false;
+}
+
+template <typename Passport>
+bool hasValidFieldEyr(Passport &&passport) {
+  if (auto it = passport.find("eyr"); it != cend(passport)) {
+    const auto year = std::stoi(it->second);
+    return year >= 2020 && year <= 2030;
+  }
+  return false;
+}
+
+template <typename Passport>
+bool hasValidFieldHgt(Passport &&passport) {
+  bool isValidHgt = false;
+  if (auto it = passport.find("hgt"); it != cend(passport)) {
+    const auto &hgt = it->second;
+    if (auto pos = hgt.find("cm"); pos != std::string::npos) {
+      const auto height = std::stoi(hgt.substr(0, pos));
+      isValidHgt = height >= 150 && height <= 193;
+    } else if (auto pos = hgt.find("in"); pos != std::string::npos) {
+      const auto number = std::stoi(hgt.substr(0, pos));
+      isValidHgt = number >= 59 && number <= 76;
+    }
+  }
+  return isValidHgt;
+}
+
+template <typename Passport>
+bool hasValidFieldHcl(Passport &&passport) {
+  if (auto it = passport.find("hcl"); it != cend(passport)) {
+    const auto &hair = it->second;
+    return hair.front() == '#' &&
+           std::find_if_not(cbegin(hair) + 1, cend(hair), ::isxdigit) == cend(hair);
+  }
+  return false;
+}
+
+template <typename Passport>
+bool hasValidFieldEcl(Passport &&passport) {
+  if (auto it = passport.find("ecl"); it != cend(passport)) {
+    const std::unordered_set<std::string> legalEyes{"amb", "blu", "brn", "gry",
+                                                    "grn", "hzl", "oth"};
+    return legalEyes.find(it->second) != end(legalEyes);
+  }
+  return false;
+}
+
+template <typename Passport>
+bool hasValidFieldPid(Passport &&passport) {
+  if (auto it = passport.find("pid"); it != cend(passport)) {
+    const auto &pid = it->second;
+    return pid.size() == 9 && std::find_if_not(cbegin(pid), cend(pid), ::isdigit) == cend(pid);
+  }
+  return false;
+}
+
 template <typename Container>
 auto crack_v1(Container &&passports) {
   auto t = HolidayBag::SportTimer("p1", "us", 1);
   return std::accumulate(cbegin(passports), cend(passports), 0u, [](auto &&sum, auto &&passport) {
-    auto isValid = (passport.size() == 8) ||
-                   (passport.size() == 7 && (passport.find("cid") == cend(passport)));
-    return isValid ? sum + 1 : sum;
+    return hasValidFieldNumber(passport) ? sum + 1 : sum;
   });
 }
 
@@ -25,66 +101,11 @@ template <typename Container>
 auto crack_v2(Container &&passports) {
   auto t = HolidayBag::SportTimer("p2", "us", 1);
   return std::accumulate(cbegin(passports), cend(passports), 0u, [](auto &&sum, auto &&passport) {
-    auto isValidFieldNumber = (passport.size() == 8) ||
-                              (passport.size() == 7 && (passport.find("cid") == cend(passport)));
-    if (!isValidFieldNumber) {
-      return sum;
-    }
-
-    auto isValidByr = false;
-    if (auto it = passport.find("byr"); it != cend(passport)) {
-      const auto year = std::stoi(it->second);
-      isValidByr = year >= 1920 && year <= 2002;
-    }
-
-    auto isValidIyr = false;
-    if (auto it = passport.find("iyr"); it != cend(passport)) {
-      const auto year = std::stoi(it->second);
-      isValidIyr = year >= 2010 && year <= 2020;
-    }
-
-    auto isValidEyr = false;
-    if (auto it = passport.find("eyr"); it != cend(passport)) {
-      const auto year = std::stoi(it->second);
-      isValidEyr = year >= 2020 && year <= 2030;
-    }
-
-    auto isValidHgt = false;
-    if (auto it = passport.find("hgt"); it != cend(passport)) {
-      const auto &hgt = it->second;
-      if (auto pos = hgt.find("cm"); pos != std::string::npos) {
-        const auto height = std::stoi(hgt.substr(0, pos));
-        isValidHgt = height >= 150 && height <= 193;
-      } else if (auto pos = hgt.find("in"); pos != std::string::npos) {
-        const auto number = std::stoi(hgt.substr(0, pos));
-        isValidHgt = number >= 59 && number <= 76;
-      }
-    }
-
-    auto isValidHcl = false;
-    if (auto it = passport.find("hcl"); it != cend(passport)) {
-      const auto &hair = it->second;
-      isValidHcl = hair.front() == '#' &&
-                   std::find_if_not(cbegin(hair) + 1, cend(hair), ::isxdigit) == cend(hair);
-    }
-
-    auto isValidEcl = false;
-    if (auto it = passport.find("ecl"); it != cend(passport)) {
-      const std::unordered_set<std::string> legalEyes{"amb", "blu", "brn", "gry",
-                                                      "grn", "hzl", "oth"};
-      isValidEcl = legalEyes.find(it->second) != end(legalEyes);
-    }
-
-    auto isValidPid = false;
-    if (auto it = passport.find("pid"); it != cend(passport)) {
-      const auto &pid = it->second;
-      isValidPid =
-          pid.size() == 9 && std::find_if_not(cbegin(pid), cend(pid), ::isdigit) == cend(pid);
-    }
-
-    const auto isValid = isValidFieldNumber && isValidByr && isValidIyr && isValidEyr &&
-                         isValidHgt && isValidHcl && isValidEcl && isValidPid;
-    return isValid ? sum + 1 : sum;
+    return (hasValidFieldNumber(passport) && hasValidFieldByr(passport) &&
+            hasValidFieldIyr(passport) && hasValidFieldHgt(passport) &&
+            hasValidFieldHcl(passport) && hasValidFieldEcl(passport) && hasValidFieldPid(passport))
+               ? sum + 1
+               : sum;
   });
 }
 
